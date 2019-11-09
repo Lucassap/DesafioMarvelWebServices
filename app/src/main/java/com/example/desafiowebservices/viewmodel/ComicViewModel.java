80aa274957090ff004d75b5f2696e1f194afccea
Robson Jonathan <rjonath@live.com>
2019-11-09 12:00:48 -0300

    quinto commit - Lucas

--- app/src/main/java/com/example/desafiowebservices/viewmodel/ComicViewModel.java
+++ app/src/main/java/com/example/desafiowebservices/viewmodel/ComicViewModel.java
@@ -5,14 +5,20 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import com.example.desafiowebservices.utils.AppUtils;
+
 import io.reactivex.disposables.CompositeDisposable;
 
-public class ComicViewModel extends AndroidViewModel {
+public class ComicViewModel extends AndroidViewModel{
 
     private CompositeDisposable disposable = new CompositeDisposable();
     public static final String PUBLIC_KEY = "6eb7e8896ec5850c52515a8a23ee97f0";
     public static final String PRIVATE_KEY = "0dd0c16fedb8a02985977eafca66b49f5e6a526f";
 
+
+    Long tsLong = System.currentTimeMillis()/1000;
+    String ts = tsLong.toString();
+    String hash = md5​(ts + ​PRIVATE_KEY ​+PUBLIC_KEY​);
 
     public ComicViewModel(@NonNull Application application) {
         super(application);
